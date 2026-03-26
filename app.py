639bd1cae88d5f5b91a4de9849a1a7bc752e9587
93i5 <84719519+lokesh09s@users.noreply.github.com>
2026-03-26 17:20:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -546,7 +546,7 @@ else:
                     url="https://openrouter.ai/api/v1/chat/completions",
                     headers={"Authorization": f"Bearer {API_KEY}"},
                     json={
-                        "model": "google/gemma-3-27b-it:free",
+                        "model": "openrouter/free",
                         "messages": history + [{"role": "user", "content": prompt}],
                         "temperature": 1.3
                     },
